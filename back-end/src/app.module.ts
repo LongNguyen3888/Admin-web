44591614356dbb11f9ce6f5345ca760e8479388d
LongNguyen3888 <longnguyen3888@gmail.com>
2025-07-02 16:02:33 +0700

    Add modules

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './modules/users/users.module';
+import { LikesModule } from './modules/likes/likes.module';
+import { MenuItemOptionsModule } from './modules/menu.item.options/menu.item.options.module';
+import { MenuItemsModule } from './modules/menu.items/menu.items.module';
+import { MenusModule } from './modules/menus/menus.module';
+import { OrderDetailModule } from './modules/order.detail/order.detail.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { RestaurantsModule } from './modules/restaurants/restaurants.module';
+import { ReviewsModule } from './modules/reviews/reviews.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigService } from '@nestjs/config';
@@ -9,6 +17,14 @@ import { ConfigService } from '@nestjs/config';
 @Module({
   imports: [
     UsersModule,
+    LikesModule,
+    MenuItemOptionsModule,
+    MenuItemsModule,
+    MenusModule,
+    OrderDetailModule,
+    OrdersModule,
+    RestaurantsModule,
+    ReviewsModule,
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
